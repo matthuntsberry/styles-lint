7d636ed632549c27260fd0b69da54eda41bcbfd7
Matthew Marshall Huntsberry <Matthew.Marshall.Huntsberry@ibm.com@matthews-mbp.austin.ibm.com>
2019-01-14 13:26:17 -0600

    Updated eslint to work with vue cli 3 and the plugin model

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,16 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
-    indent: ['error',
-      2
-    ]
-  },
-  extends: ['plugin:vue/recommended', 'plugin:prettier/recommended'
-  ],
-  // required to lint *.vue files
-  plugins: ['vue', 'prettier'
-  ],
-  // add your custom rules here
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  }
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: ['plugin:vue/essential', '@vue/prettier'],
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+	},
+	plugins: ['vue', 'prettier'],
+	parserOptions: {
+		parser: 'babel-eslint',
+		indent: ['error', 2],
+	},
 };
